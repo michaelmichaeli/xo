32a533d786c079df965c3fc005125aff4c2b680e
michaelmichaeli888 <michaelmichaeli888@gmail.com>
2021-08-09 13:54:48 +0300

    Make Homepage nicer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,18 +4,19 @@ import GroupIcon from '@material-ui/icons/Group';
 
 
 const HomePage = () => {
-
     return <div className="home-page flex column align-center justify-center">
 
-        <header className="flex column align-center justify-center">
-                <h2>Welcome To</h2>
-                <h1>Tic Tac Toe</h1>
-                <p>Play Against Ai Or Friends In Realtime Multiplayer</p>
-        </header>
-        <section className="menu flex column">
-            <a href="/ai"><SportsEsportsIcon /> Play Against Ai</a>
-            <a href="/rooms"><GroupIcon /> Play With Friends</a>
-        </section>
+        <main className="flex column align-center justify-center">
+            <h2>Welcome To</h2>
+            <h1>Tic Tac Toe</h1>
+            <p>Play Against Ai or with Friends In Realtime Multiplayer rooms</p>
+
+            <section className="menu flex column">
+                <a href="/ai"><SportsEsportsIcon /> Play Against Ai</a>
+                <a href="/rooms"><GroupIcon /> Play With Friends</a>
+            </section>
+        </main>
+
     </div>
 };
 
